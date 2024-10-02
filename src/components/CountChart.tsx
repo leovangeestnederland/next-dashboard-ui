8e3d6b00321d84328ffb9e02fac8f155a3f3b2d6
Leo <leo@marlevous.nl>
2024-10-02 19:35:10 +0200

    first diagram working

---
+++ src/components/CountChart.tsx
@@ -0,0 +1,78 @@
+"use client";
+import Image from "next/image";
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Total",
+    count: 310,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 145,
+    fill: "#c3ebfa",
+  },
+  {
+    name: "Boys",
+    count: 165,
+    fill: "#fae27c",
+  },
+];
+
+const style = {
+  top: "50%",
+  right: 0,
+  transform: "translate(0, -50%)",
+  lineHeight: "24px",
+};
+
+const CountChart = () => {
+  return (
+    <div className="bg-white rounded-xl w-full h-full p-4">
+      {/* TITLE */}
+      <div className="flex flex-row justify-between items-center">
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="more" width={20} height={20}></Image>
+      </div>
+      {/* CHART */}
+      <div className="w-full h-full">
+        <ResponsiveContainer>
+          <RadialBarChart
+            cx="50%"
+            cy="50%"
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
+            data={data}
+          >
+            <RadialBar
+              label={{ position: "insideStart", fill: "#fff" }}
+              background
+              dataKey="count"
+            />
+          </RadialBarChart>
+        </ResponsiveContainer>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-lamaSky rounded-full" />
+          <h1 className="font-bold ">1,234</h1>
+          <h2 className="text-xs text-gray-300">Boys (55%)</h2>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-lamaYellow rounded-full" />
+          <h1 className="font-bold ">1,234</h1>
+          <h2 className="text-xs text-gray-300">Girls (45%)</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default CountChart;
